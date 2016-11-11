8d57729563ba088ef3edc31941bf316a16810eb4
Nicholas Kuhl <nicksusername222@gmail.com>
2016-11-10 16:20:03 -0800

    Added 2 funcs: display seats in one row, display all prices

--- TeamC_TheaterTicketProgram_PRG410/TeamC_TheaterTicketProgram_PRG410/TeamC_TheaterTicketProgram_PRG410.cpp
+++ TeamC_TheaterTicketProgram_PRG410/TeamC_TheaterTicketProgram_PRG410/TeamC_TheaterTicketProgram_PRG410.cpp
@@ -18,6 +18,8 @@ The program gives the user seven main menu options, three of which have sub-menu
 using namespace std;
 
 void displaySeatingChart(bool seating[10][9]);
+void displayTicketPrices(double ticketPrices[10][9]);
+void displaySeatingByRow(bool seating[10][9]);
 
 int main()
 {
@@ -27,29 +29,32 @@ int main()
 
 	bool exitProgramFlag = false;
 
+	bool seatsTaken[10][9];                  // Ten rows, nine columns, true = taken, false = available (default)
+	double seatPrices[10][9]{};
+
 	while (!exitProgramFlag) {
 
 		cout << "1) Display current seating chart\n";
-		cout << "2) Purchase ticket/s\n";
-		cout << "3) Set ticket prices\n";
-		cout << "4) Display total ticket sales\n";
-		cout << "5) Display total seats available/taken\n";
-		cout << "6) Display seats available by row\n";
-		cout << "7) Exit program\n\n";
+		cout << "2) Display current ticket prices\n";
+		cout << "3) Purchase ticket/s\n";
+		cout << "4) Set ticket prices\n";
+		cout << "5) Display total ticket sales\n";
+		cout << "6) Display total seats available/taken\n";
+		cout << "7) Display seats available by row\n";
+		cout << "8) Exit program\n\n";
 
 		int menuSelection;
 		cin >> menuSelection;
-		bool seatsAvailable[10][9];                        // Ten rows, nine columns, true = taken (default), false = available
-		double seatPrices[10][9];
 
 		switch (menuSelection) {
-			case 1: displaySeatingChart(seatsAvailable); break;
-			case 2:; break;
+			case 1: displaySeatingChart(seatsTaken); break;
+			case 2: displayTicketPrices(seatPrices); break;
 			case 3:; break;
 			case 4:; break;
 			case 5:; break;
 			case 6:; break;
-			case 7: exitProgramFlag = true; break;
+			case 7: displaySeatingByRow(seatsTaken); break;
+			case 8: exitProgramFlag = true; break;
 			default: cout << "\n\nSorry, that wasn't a valid menu selection. Please choose 1-8, then press Enter.\n\n";
 		}
 	}
@@ -66,10 +71,10 @@ void displaySeatingChart(bool seating[10][9]) {
 		cout << "|  R" << i + 1 << " | ";
 		for (int j = 0; j <= 8; j++) {
 			if (seating[i][j] == true) {
-				cout << " O | ";
+				cout << " * | ";
 			}
 			else {
-				cout << " X | ";
+				cout << " # | ";
 			}
 		}
 		cout << "\n";
@@ -78,15 +83,85 @@ void displaySeatingChart(bool seating[10][9]) {
 	cout << "| R10 | ";
 	for (int j = 0; j <= 8; j++) {
 		if (seating[9][j] == true) {
-			cout << " O | ";
+			cout << " * | ";
 		}
 		else {
-			cout << " X | ";
+			cout << " # | ";
 		}
 	}
 	cout << "\n";
 	cout << "|_____|____|____|____|____|____|____|____|____|____|\n";
 	cout << "\n";
+	cout << "(Type 'c' + Enter to continue.)\n";
+	string response;
+	cin >> response;
+}
+
+void displayTicketPrices(double ticketPrices[10][9]) {
+	cout << "\n\nCurrent Ticket Prices:\n\n";
+	cout << "_______________________________________________________________________________\n";
+	cout << "|     |   C1  |   C2  |   C3  |   C4  |   C5  |   C6  |   C7  |   C8  |   C9  |\n";
+	cout << "|_____|_______|_______|_______|_______|_______|_______|_______|_______|_______|\n";
+	for (int i = 0; i <= 8; i++) {
+		cout << "|  R" << i << " | ";
+		for (int j = 0; j <= 8; j++) {
+			cout << ticketPrices[i][j] << "|";
+		}
+		cout << "\n|_____|_______|_______|_______|_______|_______|_______|_______|_______|_______|\n";
+		cout << "\n";
+	}
+	cout << "| R10 |";
+	for (int j = 0; j <= 8; j++) {
+		cout << ticketPrices[9][j] << "|";
+	}
+	cout << "\n|_____|_______|_______|_______|_______|_______|_______|_______|_______|_______|\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "(Type 'c' + Enter to continue.)\n";
+	string response;
+	cin >> response;
+}
+
+void displaySeatingByRow(bool seating[10][9]) {
+	cout << "\n\nWhich row do you want to check the seating for?\n\n";
+	int rowNumber = 0;
+	cin >> rowNumber;
+	int sumAvailableByRow = 0;
+	for (int i = 0; i < 8; i++) {
+		if (seating[rowNumber - 1][i] == false) {
+			sumAvailableByRow++;
+		}
+	}
+	cout << "\n\nThere are " << sumAvailableByRow << " seats available in row " << rowNumber << ":\n\n";
+	cout << "____________________________________________________\n";
+	cout << "|     | C1 | C2 | C3 | C4 | C5 | C6 | C7 | C8 | C9 |\n";
+	cout << "|_____|____|____|____|____|____|____|____|____|____|\n";
+	if (rowNumber < 10) {
+		cout << "|  R" << rowNumber << " |";
+		for (int i = 0; i <= 8; i++) {
+			if (seating[rowNumber - 1][i] == true) {
+				cout << "  * |";
+			}
+			else {
+				cout << "  # |";
+			}
+		}
+		cout << "\n";
+		cout << "|_____|____|____|____|____|____|____|____|____|____|\n\n";
+	}
+	else {
+		cout << "| R" << rowNumber << " |";
+		for (int i = 0; i <= 8; i++) {
+			if (seating[9][i] == true) {
+				cout << "  * |";
+			}
+			else {
+				cout << "  # |";
+			}
+		}
+		cout << "\n";
+		cout << "|_____|____|____|____|____|____|____|____|____|____|\n\n";
+	}
 	cout << "(Type 'c' + Enter to continue.)\n";
 	string response;
 	cin >> response;
